9b8c7ed7e46b8e6d0fea88b92aa5b21b64f59f05
Kugelbyte <78225057+Kugelbyte@users.noreply.github.com>
2021-09-12 15:34:22 +0530

    Update Simple_file_encryption.py

--- Simple_file_encryption.py
+++ Simple_file_encryption.py
@@ -4,6 +4,7 @@ import hashlib
 import string
 
 
+
 def encrypt_file():
     filename = input("[+] Enter the path to the file: ")   
     key = int(input("[+] Enter the key: "))
@@ -42,7 +43,7 @@ def encrypt_text():
 
     plaintext = input("[+] Enter the plaintext: ")  
     key = int(input("[+] Enter the key: "))
-    result = ''  
+    result = ''   
     for i in range(len(plaintext)):
         letter = plaintext[i]
 
@@ -54,50 +55,61 @@ def encrypt_text():
         except:
             print ("[-] Could not encrypt. Invalid plaintext!!\n")
 
-    return result
+    print(result)
 
 
 def hash_file():
     filename = input("[+] Enter the path to the file: ") 
     choice = input("[+] Enter the hashing algorithm: ")
     hash = ''
-    try:
-     match choice:
-          case 'md5':
-                with open(filename, 'r') as file:
-                    content = file.read()
-                    file.close() 
+    def if_md5():
+     with open(filename, 'r') as file:
+                content = file.read()
+                file.close() 
                 hash = hashlib.md5(content.encode())
                 print("[+] Hash calculated is ==> " + hash.hexdigest())
-    
 
-          case 'sha1':
-                with open(filename, 'r') as file:
-                    content = file.read()
-                    file.close() 
+
+    def if_sha1():
+     with open(filename, 'r') as file:
+                content = file.read()
+                file.close() 
                 hash = hashlib.sha1(content.encode())
                 print("[+] Hash calculated is ==> " + hash.hexdigest())
-     
-    
-          case 'sha256':
-                with open(filename, 'r') as file:
-                    content = file.read()
-                    file.close() 
+
+    def if_sha256():
+     with open(filename, 'r') as file:
+                content = file.read()
+                file.close() 
                 hash = hashlib.sha256(content.encode())
                 print("[+] Hash calculated is ==> " + hash.hexdigest())
+    def handle_default():
+        print("[+] Invalid selection. Try again.\n") 
+        hash_file()
     
-    except:
-      print("[-] Something went wrong. Please try again\n")
-      hash_file() 
+    def do_this(choice):
+        return
+        {
+        'md5': lambda : if_md5(),      
+       'sha1' : lambda : if_sha1(),
+       'sha256' : lambda : if_sha256(),
 
-
-
-
-
+        }.get(choice, handle_default())
+    if(os.path.isfile(filename)):
+        
+            
+            do_this = {'md5' : if_md5, 'sha1' : if_sha1, 'sha256' : if_sha256}
+            do_this.get(choice, handle_default)()
+            
+    
+    else:
+        print('[-] File does not exist. Try again.\n')
+        hash_file()
+         
 
 if __name__ == '__main__':
      
-     while True:
+ while True:
          print(
  '''
 [1] Encrypt file         
@@ -107,19 +119,19 @@ if __name__ == '__main__':
 
          Choice = input()
      
-         match Choice:
-            case '1':
+         
+         if(Choice == '1'):
                 encrypt_file()
-            case '2':
+         elif(Choice == '2'):
                 print(encrypt_text())
-            case '3':
+         elif(Choice == '3'):
                 hash_file()
-            case _:
-                print("Something went wrong")
-                continue
+         else:
+             print("[+] Something went wrong. Try")
+             continue
 
          is_end = input("Do something else (y/n): ")
-
-         if (is_end == 'n'):
-            break
-                     
+         if(is_end != 'y'):
+             break
+             
+             
